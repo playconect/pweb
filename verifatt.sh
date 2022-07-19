bdc03b4a74a236f3a78d90d475caf118c74816d3
PlaYCoNecT <andin.cell.1987@gmail.com>
2022-07-19 19:47:27 -0300

    Update verifatt.sh

--- verifatt.sh
+++ verifatt.sh
@@ -3,14 +3,14 @@ clear
 [[ -e /home/verpweb ]] && rm /home/verpweb
 wget -P /home https://github.com/playconect/pweb/raw/painel/verpweb > /dev/null 2>&1
 [[ -f "/home/verpweb" ]] && {
-	vrs1=$(sed -n '1 p' /bin/ppweb/verpweb| sed -e 's/[^0-9]//ig')
+	vrs1=$(sed -n '1 p' /bin/pweb/verpweb| sed -e 's/[^0-9]//ig')
     vrs2=$(sed -n '1 p' /home/verpweb | sed -e 's/[^0-9]//ig')
-	[[ "$vrs1" != "$vrs2" ]] && mv /home/verpweb /bin/ppweb/attpweb
+	[[ "$vrs1" != "$vrs2" ]] && mv /home/verpweb /bin/pweb/attpweb
 }
 [[ -e /home/verweb ]] && rm /home/verweb
 wget -P /home https://github.com/playconect/pweb/raw/painel/verweb > /dev/null 2>&1
 [[ -f "/home/verweb" ]] && {
-	vrs3=$(sed -n '1 p' /bin/ppweb/verweb| sed -e 's/[^0-9]//ig')
+	vrs3=$(sed -n '1 p' /bin/pweb/verweb| sed -e 's/[^0-9]//ig')
     vrs4=$(sed -n '1 p' /home/verweb | sed -e 's/[^0-9]//ig')
-	[[ "$vrs3" != "$vrs4" ]] && mv /home/verweb /bin/ppweb/attweb
+	[[ "$vrs3" != "$vrs4" ]] && mv /home/verweb /bin/pweb/attweb
 }
